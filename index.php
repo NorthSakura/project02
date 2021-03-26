99503cdcf699a7947a26f4beab97ada8e8f74999
emma-campbell <ecampb10@u.rochester.edu>
2021-03-26 11:22:16 -0400

    index page layout

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Project 2 - Welcome</title>
+    <?php include "includes/head.php" ?>
+</head>
+
+<body>
+    <header class="sticky">
+        <?php include "includes/nav.php" ?>
+    </header>
+    
+    <div class="content" id="hero">
+        <div class="grid">
+            <div class="col-1">
+                <img src="images/placeholder-image.png" alt="placeholder"/>
+            </div>
+            <div class="col-2">
+                <img src="images/placeholder-image.png" alt="placeholder"/>
+            </div>
+            <div class="col-3">
+                <img src="images/placeholder-image.png" alt="placeholder"/>
+            </div>
+            <div class="col-4">
+                <img src="images/placeholder-image.png" alt="placeholder"/>
+            </div> 
+        </div>
+        <div class="button-box">
+            <a href="neworleans.php">
+                <button>
+                    <p>LET'S GO<p>
+                </button>
+            </a>
+        </div>
+    </div>
+
+    <?php include "includes/footer.php" ?>
+</body>
+</html>

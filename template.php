0e2b92b433b3f86b5175dd28bd0c1c3524c5672f
emma-campbell <ecampb10@u.rochester.edu>
2021-03-28 14:18:11 -0400

    Made it into an "F-Layout" 😪

--- template.php
+++ template.php
@@ -27,16 +27,25 @@
 
             <section id="history">
                 <h2>History</h2>
+                <figure>
+                    <img id="sectionImg" src="images/placeholder-image.png" alt="placeholder"/>
+                </figure>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Semper quis lectus nulla at volutpat diam. Massa eget egestas purus viverra accumsan in. Scelerisque eu ultrices vitae auctor. Amet consectetur adipiscing elit ut aliquam purus sit. Id ornare arcu odio ut sem nulla pharetra diam. Ipsum a arcu cursus vitae. Purus ut faucibus pulvinar elementum. Quam id leo in vitae turpis massa sed elementum. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Imperdiet massa tincidunt nunc pulvinar sapien et. A cras semper auctor neque vitae. At risus viverra adipiscing at in tellus integer. Volutpat odio facilisis mauris sit amet. Nisl rhoncus mattis rhoncus urna neque. Tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum.</p>
             </section>
             
             <section id="attractions">
                 <h2>Attractions</h2>
+                <figure>
+                    <img id="sectionImg" src="images/placeholder-image.png" alt="placeholder"/>
+                </figure>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Semper quis lectus nulla at volutpat diam. Massa eget egestas purus viverra accumsan in. Scelerisque eu ultrices vitae auctor. Amet consectetur adipiscing elit ut aliquam purus sit. Id ornare arcu odio ut sem nulla pharetra diam. Ipsum a arcu cursus vitae. Purus ut faucibus pulvinar elementum. Quam id leo in vitae turpis massa sed elementum. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Imperdiet massa tincidunt nunc pulvinar sapien et. A cras semper auctor neque vitae. At risus viverra adipiscing at in tellus integer. Volutpat odio facilisis mauris sit amet. Nisl rhoncus mattis rhoncus urna neque. Tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum.</p>
             </section>
             
             <section id="geography">
                 <h2>Geography</h2>
+                <figure>
+                    <img id="sectionImg" src="images/placeholder-image.png" alt="placeholder"/>
+                </figure>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Semper quis lectus nulla at volutpat diam. Massa eget egestas purus viverra accumsan in. Scelerisque eu ultrices vitae auctor. Amet consectetur adipiscing elit ut aliquam purus sit. Id ornare arcu odio ut sem nulla pharetra diam. Ipsum a arcu cursus vitae. Purus ut faucibus pulvinar elementum. Quam id leo in vitae turpis massa sed elementum. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin. Imperdiet massa tincidunt nunc pulvinar sapien et. A cras semper auctor neque vitae. At risus viverra adipiscing at in tellus integer. Volutpat odio facilisis mauris sit amet. Nisl rhoncus mattis rhoncus urna neque. Tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum.</p>
             </section>
 
